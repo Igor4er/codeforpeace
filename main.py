54b0b793b249f980d0f3d094b51021435658bb7d
Igor4er <sherednihenko.ihor@gmail.com>
2023-11-11 12:41:44 +0200

    Added apply answers endpoint, removed comments

--- main.py
+++ main.py
@@ -10,10 +10,9 @@ def get_questions():
     with open("questions.json", 'r', encoding='utf-8') as json_file:
         return json.load(json_file)
 
+@app.post(path="/apply_answers")
+def apply_answers():
+    pass
 
 if __name__ == "__main__":
-    # json_data = {"questions": [1, 2, 3]}
-    # with open("questions.json", 'w') as json_file:
-    #     json.dump(json_data, json_file, indent=2)
-
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
