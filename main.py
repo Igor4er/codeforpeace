fbccc12f1416c52ba5617d3ebf700b2b9f4c8f8e
Igor4er <sherednihenko.ihor@gmail.com>
2023-11-11 13:57:59 +0200

    m

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI, Depends
 import uvicorn
 import json
+from pydantic import BaseModel
+from enum inport Enum
+
+class CategoryEnum(str, Enum):
+    wellBeing = 'Well-being'
+    physicalActivity = 'Physical Activity'
+    workAndStudying = 'Work and Studying'
+    relationships = 'Relationships'
+    socialConnections = 'Social Connections'
 
 app = FastAPI()
 
@@ -12,10 +21,15 @@ with open("questions.json", 'r', encoding='utf-8') as json_file:
 def get_questions():
     return QUESTIONS
 
+class Answer(BaseModel):
+    category: str
+    question: str
+    impact: int
 
 @app.post(path="/apply_answers")
-def apply_answers():
-    pass
+def apply_answers(answers: list[Answer]):
+    for answer in answers:
 
+        
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
