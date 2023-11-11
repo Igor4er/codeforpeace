68e7465dc7c1ea56429a75a4251d15841648bec0
EtriKodoku <volodymyrpetriv2207@gmail.com>
2023-11-11 12:32:55 +0200

    Generating JSON

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Depends
 import uvicorn
+import json
 
 app = FastAPI()
 
@@ -10,4 +11,8 @@ def get():
 
 
 if __name__ == "__main__":
+    json_data = {"questions": [1, 2, 3]}
+    with open("questions.json", 'w') as json_file:
+        json.dump(json_data, json_file, indent=2)
+
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
