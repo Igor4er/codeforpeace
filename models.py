24c250d87b9354cd115ab868c32c5ae98262d0e2
EtriKodoku <volodymyrpetriv2207@gmail.com>
2023-11-11 16:27:47 +0200

    Models for user answers

--- models.py
+++ models.py
@@ -2,7 +2,6 @@ import peewee
 
 from .database import db
 
-
 class User(peewee.Model):
     email = peewee.CharField(unique=True, index=True)
     hashed_password = peewee.CharField()
@@ -12,10 +11,14 @@ class User(peewee.Model):
         database = db
 
 
-class Item(peewee.Model):
-    title = peewee.CharField(index=True)
-    description = peewee.CharField(index=True)
-    owner = peewee.ForeignKeyField(User, backref="items")
+class Answer(peewee.Model):
+    user = peewee.CharField(primary_key=True)
+    well_being = peewee.IntegerField()
+    physical_activity = peewee.IntegerField()
+    stress = peewee.IntegerField()
+    social_connections = peewee.IntegerField()
+    work_and_studying = peewee.IntegerField()
+    date = peewee.DateTimeField()
 
     class Meta:
         database = db
